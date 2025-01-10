db3550c4a92c284afe6f5e31fe1bb44934cbd84b
eintril <178818361+eintril@users.noreply.github.com>
2025-01-09 23:42:28 -0800

    Add more explore commands for tabs and splits

--- lua/custom/commands/explore.lua
+++ lua/custom/commands/explore.lua
@@ -5,3 +5,15 @@ end, {})
 vim.api.nvim_create_user_command('Ex', function()
   vim.cmd('edit ' .. vim.fn.expand '%:p:h')
 end, {})
+
+vim.api.nvim_create_user_command('Tex', function()
+  vim.cmd('tabedit ' .. vim.fn.expand '%:p:h')
+end, {})
+
+vim.api.nvim_create_user_command('Vex', function()
+  vim.cmd('vsplit ' .. vim.fn.expand '%:p:h')
+end, {})
+
+vim.api.nvim_create_user_command('Sex', function()
+  vim.cmd('split ' .. vim.fn.expand '%:p:h')
+end, {})
