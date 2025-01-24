eda0f8d4999b6c1e1c1546bbfa6be4883bd7b57d
eintril <178818361+eintril@users.noreply.github.com>
2025-01-24 11:43:07 -0800

    Add keymaps for treesitter selection

--- init.lua
+++ init.lua
@@ -958,9 +958,10 @@ require('lazy').setup({
       incremental_selection = {
         enable = true,
         keymaps = {
+          init_selection = '<CR>',
+          scope_incremental = '<CR>',
           node_incremental = 'v',
           node_decremental = 'V',
-          scope_incremental = '<CR>',
         },
       },
     },
